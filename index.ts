6a8994b5d5ce587bcad4e12add4de741e450db8f
SabaManjavidze <sabamanjavidze@gmail.com>
2023-12-28 02:05:32 +0400

    test passed. adjusted to correct schedule

--- index.ts
+++ index.ts
@@ -5,14 +5,14 @@ import { schedule } from "node-cron";
 const app = express();
 const port = 3000;
 
-app.post("/cron", (req, res) => {
-  main();
-  res.send(200);
-});
-app.get("/", (req, res) => res.send("hello"));
+// app.post("/cron", (req, res) => {
+//   main();
+//   res.send(200);
+// });
+app.get("/", (req, res) => res.sendStatus(200).json({ message: "hello" }));
 
-// schedule("0 6-23/6 * * *", main);
-schedule("*/3 * * * *", main);
+schedule("0 6-23/6 * * *", main);
+// schedule("*/3 * * * *", main);
 // schedule("* * * * *", () => console.log("hello"));
 app.listen(process.env.PORT || port, () =>
   console.log(`Listening on port ${port}`)
