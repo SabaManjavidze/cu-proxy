1eea6b254cb1319f9e91be31206b138df295280c
SabaManjavidze <sabamanjavidze@gmail.com>
2023-12-28 21:08:40 +0400

    endpoint

--- index.ts
+++ index.ts
@@ -5,13 +5,13 @@ import { schedule } from "node-cron";
 const app = express();
 const port = 3000;
 
-// app.post("/cron", (req, res) => {
-//   main();
-//   res.send(200);
-// });
+app.post("/cron", (req, res) => {
+  main();
+  res.send(200);
+});
 app.get("/", (req, res) => res.sendStatus(200));
 
-schedule("0 12-23/3 * * *", main);
+// schedule("0 12-23/3 * * *", main);
 // schedule("*/3 * * * *", main);
 // schedule("* * * * *", () => console.log("hello"));
 app.listen(process.env.PORT || port, () =>
